38ebaa9aac8ecad6e8da75b8c25e0a505721d997
Nir Boger <niboger@microsoft.com>
2021-11-25 08:43:34 +0200

    Add project files.

---
+++ DummyTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTestPlayground
+{
+    [TestClass]
+    public class DummyTests
+    {
+        [TestMethod]
+        public void A_B_C()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
